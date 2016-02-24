43b3ca096c95cd25b21e4e6303a0123c99a9e133
Blake Scott and Jordan Meier <jbirdmeier@gmail.com>
2016-02-24 15:10:14 -0800

    add routes to app.php

--- app/app.php
+++ app/app.php
@@ -1,10 +1,11 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/ClassName.php";
+    require_once __DIR__."/../src/Restaurant.php";
+    require_once __DIR__."/../src/Cuisine.php";
 
     $app = new Silex\Application();
 
-    $server = 'mysql:host=localhost;dbname=to_do';
+    $server = 'mysql:host=localhost;dbname=best_restaurants';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -14,7 +15,38 @@
     ));
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->get("/restaurants", function() use ($app) {
+        return $app['twig']->render('restaurants.html.twig', array('restaurants' => Restaurant::getAll()));
+    });
+
+    $app->post("/restaurants", function() use ($app) {
+        $name = $_POST['name'];
+        $happy_hour = $_POST['happy_hour'];
+        $address = $_POST['address'];
+        $cuisine_id = $_POST['cuisine_id'];
+        $restaurant = new Restaurant($id= null, $name, $happy_hour, $address, $cuisine_id);
+        $restaurant->save();
+        $cuisine = Cuisine::find($cuisine_id);
+        var_dump($restaurant);
+        return $app['twig']->render('cuisines.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+    });
+
+    $app->get("/cuisines", function() use ($app) {
+        return $app['twig']->render('cuisines.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->get("/cuisines/{id}", function($id) use ($app) {
+        $cuisine = Cuisine::find($id);
+        return $app['twig']->render('cuisines.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+    });
+
+    $app->post("/cuisines", function() use ($app) {
+        $cuisine = new Cuisine($_POST['type']);
+        $cuisine->save();
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
     });
 
 

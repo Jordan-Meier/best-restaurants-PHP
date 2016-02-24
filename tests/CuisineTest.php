b09e37d0fc3bfb444a31076155676de328f61b90
Blake Scott and Jordan Meier <blakerscott@gmail.com>
2016-02-24 09:05:45 -0800

    add test files for Cuisine and Restaurant

---
+++ tests/CuisineTest.php
@@ -0,0 +1,14 @@
+<?php
+
+    require_once "src/Cuisine.php";
+
+    class CuisineTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_method_breakitdown()
+        {
+
+        }
+    }
+
+?>

a341007d6d73200a73bcd8304fb766607d5b8f3d
Blake Scott and Jordan Meier <blakerscott@gmail.com>
2016-02-24 09:38:58 -0800

    add save, getAll, deleteAll methods to Cuisine class

--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -1,13 +1,73 @@
 <?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
 
     require_once "src/Cuisine.php";
+    require_once "src/Restaurant.php";
+
+    $server = 'mysql:host=localhost;dbname=best_restaurants_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
     class CuisineTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_method_breakitdown()
+        protected function tearDown()
         {
+          Cuisine::deleteAll();
+        //   Restaurant::deleteAll();
+        }
 
+        function test_save()
+        {
+            //Arrange
+            $type = "Thai";
+            $test_Cuisine = new Cuisine($type);
+            $test_Cuisine->save();
+
+            //Act
+            $result = Cuisine::getAll();
+
+            //Assert
+            $this->assertEquals($test_Cuisine, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $type = "Thai";
+            $type2 = "Indian";
+            $test_Cuisine = new Cuisine($type);
+            $test_Cuisine->save();
+            $test_Cuisine2 = new Cuisine($type2);
+            $test_Cuisine2->save();
+
+            //Act
+            $result = Cuisine::getAll();
+
+            //Assert
+            $this->assertEquals([$test_Cuisine, $test_Cuisine2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $type = "Thai";
+            $type2 = "Indian";
+            $test_Cuisine = new Cuisine($type);
+            $test_Cuisine->save();
+            $test_Cuisine2 = new Cuisine($type2);
+            $test_Cuisine2->save();
+
+            //Act
+            Cuisine::deleteAll();
+            $result = Cuisine::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
         }
     }
 
